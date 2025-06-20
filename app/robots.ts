f2cdb526a5c936911be4cbe800ef2244ab65f6b9
Jesse <jesseneumann@gmail.com>
2025-06-20 09:13:46 -0600

    fix: dont use vercel url for site map and robots

--- app/robots.ts
+++ app/robots.ts
@@ -1,16 +1,12 @@
 import type { MetadataRoute } from 'next';
 
-const defaultUrl = process.env.VERCEL_URL
-  ? `https://${process.env.VERCEL_URL}`
-  : 'http://localhost:3000';
-
 export default function robots(): MetadataRoute.Robots {
   return {
     rules: {
       userAgent: '*',
       allow: '/',
-      disallow: '/dashboard/',
+      disallow: ['/auth/', '/dashboard/'],
     },
-    sitemap: `${defaultUrl}/sitemap.xml`,
+    sitemap: `https://remote-mcp-servers.com/sitemap.xml`,
   };
 }
--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,13 +1,9 @@
 import type { MetadataRoute } from 'next';
 
-const defaultUrl = process.env.VERCEL_URL
-  ? `https://${process.env.VERCEL_URL}`
-  : 'http://localhost:3000';
-
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: defaultUrl,
+      url: 'https://remote-mcp-servers.com/',
       lastModified: new Date(),
       changeFrequency: 'weekly',
       priority: 1,
