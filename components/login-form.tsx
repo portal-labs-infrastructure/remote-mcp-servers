df32059354583590b4a3f477ea4e28b0d0818e37
Jesse <jesseneumann@gmail.com>
2025-06-20 10:09:07 -0600

    fix: use h1 tag for login and signup pages

--- components/login-form.tsx
+++ components/login-form.tsx
@@ -8,7 +8,6 @@ import {
   CardContent,
   CardDescription,
   CardHeader,
-  CardTitle,
 } from '@/components/ui/card';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
